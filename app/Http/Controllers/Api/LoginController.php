c6f14479a1e171e04ca7f1efb7f64aa715f7a65b
Izzulhaq Fawwaz Syauqiy <izzulhaq219@gmail.com>
2025-03-25 17:37:15 +0700

    add login feature

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized. Invalid email or password',
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login success',
+            'user'    => auth()->guard('api')->user(),
+            'token'   => $token
+        ], 200);
+    }
+}
